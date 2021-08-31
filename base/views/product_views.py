aaeebef8f3f8fa485fddc00d4a8abcf15e93a7a5
Justin <48664609+GlitchicaL@users.noreply.github.com>
2021-08-31 12:08:07 -0400

    Staticfiles fix

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 # Import Models
@@ -22,3 +23,11 @@ def getProduct(request, id):
     product = Product.objects.get(_id=id)
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteProduct(request, id):
+    product = Product.objects.get(_id=id)
+    product.delete()
+    return Response('Product Deleted')
